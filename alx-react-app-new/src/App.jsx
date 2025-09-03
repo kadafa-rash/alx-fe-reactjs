6ae27abe377f072c1097594b8acad82e06f6916a
faith jepngetich <fleviahfay@gmail.com>
2025-09-03 12:03:47 +0300

    add couter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,6 +23,7 @@ function App() {
         age="25" 
         bio="Loves hiking and photography" 
       />
+      <Counter />
     </div>
       <div>
         <a href="https://vite.dev" target="_blank">
