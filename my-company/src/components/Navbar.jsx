9838ede4f1f71d4c45bda11702e787a376df6429
faith jepngetich <fleviahfay@gmail.com>
2025-09-04 15:47:23 +0300

    add inline link

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,11 +3,39 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333' }}>
-      <Link to="/" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Contact</Link>
+    <nav
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '15px',
+        backgroundColor: '#333',
+      }}
+    >
+      <Link
+        to="/"
+        style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}
+      >
+        Home
+      </Link>
+      <Link
+        to="/about"
+        style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}
+      >
+        About
+      </Link>
+      <Link
+        to="/services"
+        style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}
+      >
+        Services
+      </Link>
+      <Link
+        to="/contact"
+        style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}
+      >
+        Contact
+      </Link>
     </nav>
   );
 }
