955aba1e9366b715866cc494d2309d04a65fb677
faith jepngetich <fleviahfay@gmail.com>
2025-09-06 18:18:02 +0300

    rename

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,8 +1,16 @@
-// src/ProfilePage.jsx
-import UserDetails from './UserDetails';
+import React, { useContext } from 'react';
+import { UserContext } from '../context/UserContext'; // adjust path if needed
 
-function ProfilePage() {
-  return <UserDetails />;
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default ProfilePage;
+export default UserProfile;
