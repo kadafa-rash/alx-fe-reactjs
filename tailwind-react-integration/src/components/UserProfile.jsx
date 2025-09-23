e2e2d85e78527ef3e2730f87d3c3908a23e9f2ce
faith jepngetich <fleviahfay@gmail.com>
2025-09-23 11:15:40 +0300

    add responsive design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
-      <img className="w-36 h-36 rounded-full mx-auto"
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg text-center">
+      <img className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto"
        src="https://via.placeholder.com/150"
         alt="User" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-base text-gray-600">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-sm md:text-base text-gray-600">
       Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
