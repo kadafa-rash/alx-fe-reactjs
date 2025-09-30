bbe6f8af19dfe6f4716c7872359e0dc28e0eb4c6
faith jepngetich <fleviahfay@gmail.com>
2025-09-30 10:40:16 +0300

    add public/inex.html

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false, // You can change to 'media' or 'class' later if needed
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./index.html', './public/index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // You can set this to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
