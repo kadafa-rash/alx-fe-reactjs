5c75a2b4c799518e10cfb252b4f7ec91d037bc09
faith jepngetich <fleviahfay@gmail.com>
2025-09-30 10:18:50 +0300

    initial commit

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-500 p-4">
+      Hello, Recipe Sharing Platform!
+    </div>
+  );
+}
+
+export default App;
+
---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // You can change to 'media' or 'class' later if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
