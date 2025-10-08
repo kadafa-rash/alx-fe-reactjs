712bb69355846ae0c8e21fb8b35d276a14eb40b8
faith jepngetich <fleviahfay@gmail.com>
2025-10-08 11:36:38 +0300

    changed the value

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,35 +1,28 @@
 import React, { useState } from "react";
 
 const RegistrationForm = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
+  
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     
-    if (!formData.username || !formData.email || !formData.password) {
+    if (!username || !email || !password) {
       setError("All fields are required!");
       return;
     }
 
     setError("");
-    console.log("Submitted Data:", formData);
+    console.log("Submitted Data:", { username, email, password });
 
     
     fetch("https://jsonplaceholder.typicode.com/users", {
       method: "POST",
-      body: JSON.stringify(formData),
+      body: JSON.stringify({ username, email, password }),
       headers: { "Content-Type": "application/json" },
     })
       .then((res) => res.json())
@@ -45,8 +38,8 @@ const RegistrationForm = () => {
           type="text"
           name="username"
           placeholder="Username"
-          value={formData.username} 
-          onChange={handleChange}
+          value={username}     
+          onChange={(e) => setUsername(e.target.value)}
         />
         <br /><br />
 
@@ -54,8 +47,8 @@ const RegistrationForm = () => {
           type="email"
           name="email"
           placeholder="Email"
-          value={formData.email}  
-          onChange={handleChange}
+          value={email}        
+          onChange={(e) => setEmail(e.target.value)}
         />
         <br /><br />
 
@@ -63,8 +56,8 @@ const RegistrationForm = () => {
           type="password"
           name="password"
           placeholder="Password"
-          value={formData.password}  
-          onChange={handleChange}
+          value={password}     
+          onChange={(e) => setPassword(e.target.value)}
         />
         <br /><br />
 
